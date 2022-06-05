132b8b728e369cde9c1be44d9abaa3af4b408bbe
csluikidikilest <csluikidikilest@gmail.com>
2022-06-05 07:44:40 +0200

    Configuration Loading implementation

--- src/main/java/org/qazima/habari/pluginsystem/IReadOnlyConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.qazima.habari.pluginsystem;
-
-public interface IReadOnlyConfiguration {
-    boolean getAllowGet();
-    String getMetadataUri();
-    String getType();
-    String getRestUri();
-    void LoadFromJtoken(int defaultPageSize, boolean allowGet);
-}
