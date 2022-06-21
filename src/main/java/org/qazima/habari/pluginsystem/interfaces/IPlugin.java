eea9baea950fb0d1380e736ccd0905778a1cfc56
Csluikidikilest <csluikidikilest@gmail.com>
2022-06-21 06:26:14 +0200

    Clean up & rename

--- src/main/java/org/qazima/habari/pluginsystem/interfaces/IPlugin.java
+++ src/main/java/org/qazima/habari/pluginsystem/interfaces/IPlugin.java
@@ -3,13 +3,12 @@ package org.qazima.habari.pluginsystem.interfaces;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.sun.net.httpserver.HttpContext;
-import org.qazima.habari.pluginsystem.library.ContentManager;
+import org.qazima.habari.pluginsystem.library.Content;
 
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public interface IPlugin {
     IConfiguration getConfiguration();
     boolean Configure(JsonNode node, int defaultPageSize, boolean isGetAllowed, boolean isDeleteAllowed, boolean isPostAllowed, boolean isPutAllowed);
-    int Process(HttpContext httpContext, ContentManager contentManager);
-    int ProcessConfigure(HttpContext httpContext, ContentManager contentManager);
-    int ProcessMetadata(HttpContext httpContext, ContentManager contentManager);
+    int Process(HttpContext httpContext, Content content);
+    int ProcessConfigure(HttpContext httpContext, Content content);
+    int ProcessMetadata(HttpContext httpContext, Content content);
 }
