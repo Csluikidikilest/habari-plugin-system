d1cbc3dfbd12e2883d85ec152545fc815ec4a5d0
csluikidikilest <csluikidikilest@gmail.com>
2022-07-03 08:49:20 +0200

    Loading configuration and finish ContentManager

--- src/main/java/org/qazima/habari/pluginsystem/interfaces/IPlugin.java
+++ src/main/java/org/qazima/habari/pluginsystem/interfaces/IPlugin.java
@@ -1,14 +1,13 @@
 package org.qazima.habari.pluginsystem.interfaces;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
 import org.qazima.habari.pluginsystem.library.Content;
 
 public interface IPlugin {
     IReadOnlyConfiguration getConfiguration();
     boolean configure(JsonNode node, int defaultPageSize, boolean isGetAllowed, boolean isDeleteAllowed, boolean isPostAllowed, boolean isPutAllowed);
-    int process(HttpContext httpContext, Content content);
-    int processConfigure(HttpContext httpContext, Content content);
-    int processMetadata(HttpContext httpContext, Content content);
+    int process(HttpExchange exchange, Content content);
+    int processConfigure(HttpExchange exchange, Content content);
+    int processMetadata(HttpExchange exchange, Content content);
 }
